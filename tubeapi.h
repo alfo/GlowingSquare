dbb9e383002e97ebb886813fdc982dc30f8d038d
Alex Forey <me@alexforey.com>
2020-05-30 17:13:37 +0100

    Add support for showing the platform name

--- tubeapi.h
+++ tubeapi.h
@@ -16,48 +16,69 @@
 
 #define TFL_APP_ID "5ee457fb"
 #define TFL_APP_KEY "7bb4c9392c555bc1332b85f1714c2acf"
-#define STATION_ID "940GZZLUACY"
-#define TUBE_LINE "northern"
-#define DIRECTION "inbound"
+#define STATION_ID "940GZZLUACY"  // https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/StopPoint/StopPoint_Search
+#define TUBE_LINE "northern"      // https://api.tfl.gov.uk/swagger/ui/index.html?url=/swagger/docs/v1#!/Line/Line_Arrivals
+#define DIRECTION "inbound" // Options are "outbound", "inbound", or "" for both
 
+#define SHOW_PLATFORM // If you want to include the departure platform in the scrolling text
+
+// Struct to store the information about each
+// train departure
 struct Departure {
   int timeToStation;
   const char* towards;
+  const char* platform;
 };
 
+// Keep track of whether our last request failed
+boolean offline = true;
 
-void displayArrivalEntry(int line, JsonVariant entry) {
-
-  Serial.println();
-  
-}
-
+// Function that runs from loop() every 60 seconds
 boolean downloadAndDisplayTubeInfo() {
 
   HTTPClient http;
 
-  char requestURL[256];
-  
+  // This is the API endpoint that we fetch new departures for our station from
+  char requestURL[256];  
   sprintf(requestURL, "https://api.tfl.gov.uk/Line/%s/Arrivals/%s?direction=%s&app_key=%s&app_id=%s", TUBE_LINE, STATION_ID, DIRECTION, TFL_APP_KEY, TFL_APP_ID);
 
+  // Fetch the data from the server
   http.begin(requestURL);
   int httpCode = http.GET();
 
+  // Check that the server gave a valid response
   if (httpCode != 200) {
-    Serial.printf("Failed to get info with HTTP Code %i\n", httpCode);
+    Serial.printf("Failed to get info with HTTP Code %i\n", httpCode); // Code -11 means the request timed out
+    
+    // Mark us as offline so the little icon will be drawn next time
+    offline = true;
     return false;
+    
+  } else {
+
+    // All is good
+    offline = false;
   }
 
+  
   String payload = http.getString();
 
   if (payload.length() == 0) {
-    Serial.println("No data received from TFL");
+
+    // Sometimes there will just be no departures, cos the station is closed etc.
+    Serial.println("No data received from TfL");
     return false;
   }
 
+  // Close the HTTPClient to free memory
   http.end();
 
-  Serial.println("Downloaded info from TFL");
+  /*
+   * Because the JSON response from TfL can be absolutely huge sometimes (especially if you want 
+   * to see departures in both directions
+   * 
+   */
+
   
   StaticJsonDocument<160> filter;
   filter[0]["lineName"] = true;
@@ -72,14 +93,14 @@ boolean downloadAndDisplayTubeInfo() {
   StaticJsonDocument<2048> json;
   deserializeJson(json, payload, DeserializationOption::Filter(filter));
 
-  serializeJsonPretty(json, Serial);
-
   int departureCount = 0;
 
   for (JsonVariantConst v : json.as<JsonArray>()) {
-    departures.push_back({v["timeToStation"], v["towards"]});
+    departures.push_back({v["timeToStation"], v["towards"], v["platformName"]});
     departureCount++;
   }
+
+  Serial.printf("Downloaded %i items from TfL\n", departureCount);
 
   std::sort(departures.begin(), departures.end(), [](const Departure &a, const Departure &b) {
     return a.timeToStation < b.timeToStation;
@@ -102,10 +123,15 @@ boolean downloadAndDisplayTubeInfo() {
     char formattedMins[10];
     sprintf(formattedMins, "%im", mins);
 
-    Serial.print(formattedMins);
-    Serial.println(departures[i].towards);
+    Serial.printf("%s %s %s\n", formattedMins, departures[i].towards, departures[i].platform);
 
-    drawStaticAndScrollingText(i*8, 50, formattedMins, departures[i].towards, 255, 80, 0, 150, 150, 150);
+    #ifdef SHOW_PLATFORM
+      String scrollingText = (String)departures[i].towards + " " + (String)departures[i].platform;
+    #else
+      String scrollingText = (String)departures[i].towards
+    #endif
+    
+    drawStaticAndScrollingText(i*8, 50, formattedMins, scrollingText, 255, 80, 0, 150, 150, 150);
 
     delay(1000);
     
