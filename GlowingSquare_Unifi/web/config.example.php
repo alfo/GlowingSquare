eef65034cb852b57eeab92aae9afd264e30164cf
Alex Forey <me@alexforey.com>
2020-06-13 13:16:26 +0100

    Added sample config

---
+++ GlowingSquare_Unifi/web/config.example.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) 2017, Art of WiFi
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this package in the file LICENSE.md
+ *
+ */
+
+/**
+ * Controller configuration
+ * ===============================
+ * Copy this file to your working directory, rename it to config.php and update the section below with your UniFi
+ * controller details and credentials
+ */
+$controlleruser     = ''; // the user name for access to the UniFi Controller
+$controllerpassword = ''; // the password for access to the UniFi Controller
+$controllerurl      = 'https://10.0.1.2:8443'; // full url to the UniFi Controller, eg. 'https://22.22.11.11:8443', for UniFi OS-based
+                          // controllers a port suffix isn't required, no trailing slashes should be added
+$controllerversion  = ''; // the version of the Controller software, e.g. '4.6.6' (must be at least 4.0.0)
+
+/**
+ * set to true (without quotes) to enable debug output to the browser and the PHP error log
+ */
+$debug = false;
---
+++ GlowingSquare_Unifi/web/index.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * PHP API usage example
+ *
+ * contributed by: @gahujipo
+ * description: example to pull connected users and their details from the UniFi controller and output the results
+ *              in JSON format
+ */
+/**
+ * include the config file (place your credentials etc there if not already present)
+ * see the config.template.php file for an example
+ */
+require_once('config.php');
+require_once('src/Client.php');
+
+function formatBytes($bytes, $precision = 2) {
+    $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = min($pow, count($units) - 1);
+
+    // Uncomment one of the following alternatives
+    // $bytes /= pow(1024, $pow);
+    $bytes /= (1 << (10 * $pow));
+
+    return round($bytes, $precision) . $units[$pow];
+}
+
+/**
+ * the short name of the site which you wish to query
+ */
+$site_id = 'default';
+
+/**
+ * initialize the UniFi API connection class and log in to the controller and pull the requested data
+ */
+$unifi_connection = new UniFi_API\Client($controlleruser, $controllerpassword, $controllerurl, $site_id, $controllerversion);
+$set_debug_mode   = $unifi_connection->set_debug($debug);
+$loginresults     = $unifi_connection->login();
+$clients_array    = $unifi_connection->list_clients();
+$minute_stats     = $unifi_connection->stat_5minutes_site();
+$daily_stats      = $unifi_connection->stat_daily_site();
+
+$out = [];
+$out['clients'] = 0;
+$out['guests'] = 0;
+$out['wireless'] = 0;
+$out['wired'] = 0;
+$raw_month_tx = 0;
+$raw_month_rx = 0;
+
+/*
+  Monthly Stats
+*/
+
+// Gather the monthly totals of WAN up and down
+$current_month = date('m', time());
+
+foreach ($daily_stats as $stat) {
+
+  // Only use the data if it's from this month
+  if (date('m', $stat->time) == $current_month) {
+    $raw_month_tx += $stat->{'wan-tx_bytes'};
+    $raw_month_rx += $stat->{'wan-rx_bytes'};
+  }
+
+}
+
+// Format the byte counts into something more readable
+$out['month_rx'] = formatBytes($raw_month_tx, 0);
+$out['month_tx'] = formatBytes($raw_month_rx, 0);
+
+/*
+  Minute-by-minute graph
+*/
+$out['graph'] = [];
+// Each entry will be one line of pixels on the graph
+// So we only want the last 64 entries
+$graph_width = $_GET['width'];
+$graph_height = $_GET['height'];
+
+
+// This is based on some status from my house
+$max_graph = 300000000;
+
+// Use the last 100 datapoints to figure out the max height of the graph
+$n = 100;
+foreach (array_slice($minute_stats, -$n, $n) as $stat) {
+  if ($stat->wlan_bytes > $max_graph) $max_graph = $stat->wlan_bytes;
+}
+
+// Use the display width number of datapoints to draw the graph
+
+foreach (array_slice($minute_stats, -$graph_width, $graph_width) as $stat) {
+
+  $pixel_height = ceil(($stat->wlan_bytes / $max_graph) * $graph_height);
+  array_push($out['graph'], $pixel_height);
+
+}
+
+//$out['max'] = $max_graph;
+/*
+  Client Stats
+*/
+
+// Count the different types of clients
+foreach ($clients_array as $client) {
+
+  $out['clients']++;
+
+  if ($client->is_guest) $out['guests']++;
+  if ($client->is_wired) $out['wired']++;
+    else $out['wireless']++;
+
+}
+
+/**
+ * output the results in JSON format
+ */
+header('Content-Type: application/json; charset=utf-8');
+//echo json_encode($minute_stats, JSON_PRETTY_PRINT);
+echo json_encode($out);
